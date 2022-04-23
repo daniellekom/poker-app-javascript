1a3d3404297df2d7e368be05e1ffa681c565477c
Danielle Kom <daniellekom1@gmail.com>
2022-04-23 16:56:31 -0400

    created deck of cards that output cards between 1-5 3 tests passed 2 failed so far

--- cards.js
+++ cards.js
@@ -1,12 +1,39 @@
 function deal(numCardsToDeal) {
   // Write your code to generate a deal of cards in this function
-  return [];
+  const values = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+  ];
+  const suits = ["*", "^", "%", "@"];
+
+  let shuffledDeck = values
+  .reduce((acc,curr) =>{
+    for (const suit of suits){
+      acc.push(curr + suit);
+    }
+    return acc;
+  }, [])
+  .sort(() =>(Math.random() > 0.5 ? 1 : -1))
+  .slice(0, numCardsToDeal)
+
+  return shuffledDeck;
 }
 
 function report(cards) {
   // Do the required reporting on a given array of cards (just print
   // to the console, no need to get fancy)
-  console.log('Here is example reporting output');
+  console.log(`You drew: (${cards})`);
 }
 
 class NotEnoughCardsError extends Error {
--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const {deal, report} = require("./cards");
 
 if (process.argv.length !== 3) {
-  console.log('This program must be run using the following form:\nnode cards.js <num-cards-to-deal>');
+  console.log('You must ask for a number between 1 and 5');
   process.exit(1);
 }
 
 const numCards = parseInt(process.argv[2]);
 if (isNaN(numCards)) {
-  console.log('The argument to this program must be an integer');
-  process.exit(1);
+  console.log('You must ask for a number between 1 and 5')
+ process.exit(1);
+}
+
+if (numCards < 1 || numCards > 5) {
+  console.log( 'You must ask for a number between 1 and 5')
 }
 
 const cards = deal(numCards);
