bda2075e3857fac584212c8e93b004c731d0336b
Danielle Kom <daniellekom1@gmail.com>
2022-04-25 16:06:07 -0400

    Revert "added folder for special hands"
    This reverts commit c7cb4e63c8797323c2357fb6f45ba24fe94a28c2.

--- cards.js
+++ cards.js
@@ -1,5 +1,3 @@
-const {getSpecialHand} = require ("./specialHands")
-
 function deal(numCardsToDeal) {
   // Write your code to generate a deal of cards in this function
   const values = [
@@ -41,6 +39,23 @@ function report(cards) {
     console.log(`You drew: (${formattedCards})` );
 }
 
+function getSpecialHand(cards){
+  if(isRoyalFlush(cards)) return 'royal flush'
+
+}
+
+function isRoyalFlush(cards) {
+  const sameSuit = new Set(cards.map((card) => card.suit)).size === 1;
+  if (!sameSuit) return false;
+
+  const royalNumbers =
+    cards
+      .map((card) => card.value)
+      .sort()
+      .join() === "10,A,J,K,Q";
+  if (royalNumbers) return true;
+  return false;
+}
 
 class NotEnoughCardsError extends Error {
   constructor() {
